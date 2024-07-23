a660b32e273a130144cf536349c90c33896de91b
Depanshu Sani <depanshus@iiitd.ac.in>
2024-07-24 00:46:25 +0530

    progress bar updates

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import zipfile
 from werkzeug.utils import secure_filename
 import subprocess
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
@@ -27,13 +28,18 @@ os.makedirs(TAGGED_FOLDER, exist_ok=True)
 os.makedirs(DETECT_FOLDER, exist_ok=True)
 os.makedirs(ZIP_FOLDER, exist_ok=True)
 
+n_files = 0
+progress = 0
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def clear_directory(directory):
     if os.path.exists(directory):
         shutil.rmtree(directory)
     os.makedirs(directory, exist_ok=True)
+
 
 def zip_tagged_images():
     zip_filename = os.path.join(app.config['ZIP_FOLDER'], 'tagged_images.zip')
@@ -44,6 +50,7 @@ def zip_tagged_images():
                 arcname = os.path.relpath(file_path, start=app.config['TAGGED_FOLDER'])
                 zipf.write(file_path, arcname)
     return zip_filename
+
 
 @app.route('/')
 def index():
@@ -56,52 +63,82 @@ def index():
         categorized_images[category] = images
     with open('species_mapping.pkl', 'rb') as f:
         species_mapping = pickle.load(f)
+    species_mapping = {k.strip(): species_mapping[k] for k in species_mapping.keys()}
     return render_template('index.html', categories=categories, categorized_images=categorized_images, species_mapping=species_mapping)
+
 
 @app.route('/', methods=['POST'])
 def upload_files():
+    global n_files, progress
     if 'file' not in request.files:
         flash('No file part')
         return redirect(request.url)
     
     files = request.files.getlist('file')
-    
+    n_files = len(files)
+    progress = 0
     # Clear directories
-    clear_directory(app.config['UPLOAD_FOLDER'])
     clear_directory(app.config['TAGGED_FOLDER'])
+    clear_directory(app.config['UPLOAD_FOLDER'])
     clear_directory(app.config['DETECT_FOLDER'])
-    
-    for file in files:
+    batch_size = min(10, n_files // 3)
+    for i, file in enumerate(files):
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
             print(f"Saved file to {file_path}")
+        if i % batch_size == 0:
+            # Run detect.py
+            detect_process = subprocess.run(["python", "detect.py"], capture_output=True, text=True)
+            print(detect_process.stdout)
+            print(detect_process.stderr)
 
-    # Run detect.py
+            # Run tag_images.py
+            tag_process = subprocess.run(["python", "tag_images.py"], capture_output=True, text=True)
+            print(tag_process.stdout)
+            print(tag_process.stderr)
+
+            clear_directory(app.config['UPLOAD_FOLDER'])
+            clear_directory(app.config['DETECT_FOLDER'])
+            progress += batch_size
+    # Run detect.py for remaining images
     detect_process = subprocess.run(["python", "detect.py"], capture_output=True, text=True)
     print(detect_process.stdout)
     print(detect_process.stderr)
 
-    # Run tag_images.py
+    # Run tag_images.py for remaining images
     tag_process = subprocess.run(["python", "tag_images.py"], capture_output=True, text=True)
     print(tag_process.stdout)
     print(tag_process.stderr)
+    n_files = 0
+    progress = 0
 
     return redirect(url_for('index'))
+
+
+@app.route('/get_progress/')
+def get_progress():
+    global progress, n_files
+
+    return str(np.round(progress * 100 / n_files, 2)) if n_files > 0 else '0'
+
 
 @app.route('/download')
 def download_files():
     zip_filepath = zip_tagged_images()
     return send_from_directory(directory=app.config['ZIP_FOLDER'], path='tagged_images.zip', as_attachment=True)
 
+
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory('static', filename)
 
+
 @app.route('/display_image/<path:filename>')
 def display_image(filename):
     return send_from_directory(TAGGED_FOLDER, filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
